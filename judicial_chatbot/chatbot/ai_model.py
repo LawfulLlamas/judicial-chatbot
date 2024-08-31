c8c463fe16d2502ef7590594aef4cc6c46181617
Rajat Disawal <kashewknutt@gmail.com>
2024-08-31 11:14:00 +0530

    updates

--- judicial_chatbot/chatbot/ai_model.py
+++ judicial_chatbot/chatbot/ai_model.py
@@ -2,11 +2,12 @@ import requests
 
 class JudicialChatbot:
     def __init__(self):
-        self.api_url = "https://0c7d-34-75-22-103.ngrok-free.app/predict"
+        self.api_url = "https://04c2-34-19-49-181.ngrok-free.app/predict"
 
     def generate_response(self, input_text):
         response = requests.post(self.api_url, json={"instruction": input_text})
         if response.status_code == 200:
+            print(response.json())
             return response.json().get('response', "No response received.")
         else:
             return "Sorry, I couldn't generate a response at this time."
